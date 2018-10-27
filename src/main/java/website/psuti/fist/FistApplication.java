6d325dd029c3402c8f391b964a2a3820f2de4131
BadProgramy <airatmyhytdinov08@gmail.com>
2018-10-27 18:10:23 +0300

    Create project

---
+++ src/main/java/website/psuti/fist/FistApplication.java
@@ -0,0 +1,12 @@
+package website.psuti.fist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistApplication.class, args);
+	}
+}
---
+++ src/test/java/website/psuti/fist/FistApplicationTests.java
@@ -0,0 +1,16 @@
+package website.psuti.fist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

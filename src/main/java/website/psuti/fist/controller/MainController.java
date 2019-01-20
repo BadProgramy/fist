7fefee4e848ff817a71eb134821fa5ca8d279044
BadProgramy <airatmyhytdinov08@gmail.com>
2019-01-20 17:12:10 +0400

    Сделал кеш

--- src/main/java/website/psuti/fist/controller/MainController.java
+++ src/main/java/website/psuti/fist/controller/MainController.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

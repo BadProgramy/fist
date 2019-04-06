a597f43b9cf949fc0043d94907ca9150e8c6d4c7
BadProgramy <airatmyhytdinov08@gmail.com>
2019-04-06 14:12:49 +0300

    Обновил скрипты

---
+++ SQLDump/SQLUpdate/fist_candidate_assignment.sql
@@ -0,0 +1,3 @@
+UPDATE fist.candidate_assignment SET course = '4', group_in_university = 'ПО-51', name = 'Зубков Евгений Романович' WHERE id = 1;
+UPDATE fist.candidate_assignment SET course = '4', group_in_university = 'ПО-52', name = 'Мухутдинов Айрат Анварович' WHERE id = 2;
+UPDATE fist.candidate_assignment SET course = '4', group_in_university = 'ПО-52', name = 'Печерин Алексей Олегович' WHERE id = 3;

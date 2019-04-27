7ae62e4151d17d827bd0d21af66ec68815eb8443
BadProgramy <airatmyhytdinov08@gmail.com>
2019-04-27 21:05:47 +0300

    Подкоректировал пользователей

--- SQLDump/SQLInsert/fist_users_role.sql
+++ SQLDump/SQLInsert/fist_users_role.sql
@@ -1,4 +1,5 @@
 INSERT INTO fist.users_role (users_id, role) VALUES (1, 'DEVELOPER');
-INSERT INTO fist.users_role (users_id, role) VALUES (55, 'ADMIN');
-INSERT INTO fist.users_role (users_id, role) VALUES (55, 'SUBSCRIBER');
-INSERT INTO fist.users_role (users_id, role) VALUES (58, 'SUBSCRIBER');
+INSERT INTO fist.users_role (users_id, role) VALUES (59, 'ADMIN');
+INSERT INTO fist.users_role (users_id, role) VALUES (59, 'MODERATOR');
+INSERT INTO fist.users_role (users_id, role) VALUES (59, 'DEVELOPER');
+INSERT INTO fist.users_role (users_id, role) VALUES (59, 'SUBSCRIBER');

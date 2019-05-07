467da21c1dc05e1c0eac7a36e8f5406d11266c14
BadProgramy <airatmyhytdinov08@gmail.com>
2019-05-07 16:43:19 +0300

    обнова запросов

--- SQLDump/SQLUpdate/fist_users_role.sql
+++ SQLDump/SQLUpdate/fist_users_role.sql
@@ -1,5 +1,4 @@
 UPDATE fist.users_role SET users_id = 1, role = 'DEVELOPER';
-UPDATE fist.users_role SET users_id = 59, role = 'ADMIN';
-UPDATE fist.users_role SET users_id = 59, role = 'MODERATOR';
-UPDATE fist.users_role SET users_id = 59, role = 'DEVELOPER';
-UPDATE fist.users_role SET users_id = 59, role = 'SUBSCRIBER';
+UPDATE fist.users_role SET users_id = 55, role = 'ADMIN';
+UPDATE fist.users_role SET users_id = 55, role = 'SUBSCRIBER';
+UPDATE fist.users_role SET users_id = 58, role = 'SUBSCRIBER';

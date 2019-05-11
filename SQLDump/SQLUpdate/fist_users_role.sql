7fd8e8eb5e4f705b2cf727fcaca4100d0bedabcc
BadProgramy <airatmyhytdinov08@gmail.com>
2019-05-11 18:58:53 +0300

    Динамичный вывод содержимое страницы

--- SQLDump/SQLUpdate/fist_users_role.sql
+++
@@ -1,4 +0,0 @@
-UPDATE fist.users_role SET users_id = 1, role = 'DEVELOPER';
-UPDATE fist.users_role SET users_id = 55, role = 'ADMIN';
-UPDATE fist.users_role SET users_id = 55, role = 'SUBSCRIBER';
-UPDATE fist.users_role SET users_id = 58, role = 'SUBSCRIBER';

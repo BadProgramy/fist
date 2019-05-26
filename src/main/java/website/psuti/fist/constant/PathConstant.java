dff8273c0d169accebb8972a2fd1c17e0a0787e7
BadProgramy <airatmyhytdinov08@gmail.com>
2019-05-26 22:25:58 +0300

    archive data download

--- src/main/java/website/psuti/fist/constant/PathConstant.java
+++ src/main/java/website/psuti/fist/constant/PathConstant.java
@@ -3,12 +3,12 @@ package website.psuti.fist.constant;
 import java.io.IOException;
 
 public enum PathConstant {
-    SAVE_PICTURE(""),//"src\\main\\resources\\downloadPictures\\
-    SAVE_PICTURE_NEWS_FACULTY(""),//src\main\resources\downloadPictures\newsFaculty\
-    SAVE_PICTURE_BEST_STUDENT(""),//src\main\resources\downloadPictures\bestStudents\
-    HTML_FILE_FOR_USER_ADD_CMS("src\\main\\resources\\downloadPictures\\sendEmailHtml\\cms.html"),//src\main\resources\downloadPictures\sendEmailHtml\cms.html
-    HTML_FILE_FOR_USER_SUBSCRIBE("src\\main\\resources\\downloadPictures\\sendEmailHtml\\subscriber.html"),//src\main\resources\downloadPictures\sendEmailHtml\subscriber.html
-    SAVE_FILE("");//files\
+    SAVE_PICTURE("DownloadArchive/"),//"src\\main\\resources\\downloadPictures\\
+    SAVE_PICTURE_NEWS_FACULTY("DownloadArchive/"),//src\main\resources\downloadPictures\newsFaculty\
+    SAVE_PICTURE_BEST_STUDENT("DownloadArchive/"),//src\main\resources\downloadPictures\bestStudents\
+    HTML_FILE_FOR_USER_ADD_CMS("DownloadArchive/cms.html"),//src\main\resources\downloadPictures\sendEmailHtml\cms.html
+    HTML_FILE_FOR_USER_SUBSCRIBE("DownloadArchive/subscriber.html"),//src\main\resources\downloadPictures\sendEmailHtml\subscriber.html
+    SAVE_FILE("DownloadArchive/");//files\
 
     private String path;
 
